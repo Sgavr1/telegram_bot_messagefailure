95b6edce266d70a21ea621e8fa307d4e8a95a390
sgavr1 <sanya.gavrilko60@gmail.com>
2025-09-13 17:26:49 +0200

    init commit

---
+++ src/main/java/com/telegram/bot/messagefailure/MessagefailureApplication.java
@@ -0,0 +1,13 @@
+package com.telegram.bot.messagefailure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessagefailureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessagefailureApplication.class, args);
+	}
+
+}
